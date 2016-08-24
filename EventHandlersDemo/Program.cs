1d5f8f6e6917f3329081f1af83f759889445ae9d
Hanuma <suryahanuma@gmail.com>
2016-08-24 14:06:57 +0530

    Initial Commit

---
+++ EventHandlersDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandlersDemo
+{
+    public delegate void DisplayTotalMarks(string name, int totalMarks);
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var stud = new UniversityStudent
+            {
+                Name = "John Doe",
+                TotalMarks = 100
+            };
+
+            //Here we change the marks...so the event "TotalMarksHandler" gets fired again. 
+           
+            stud.TotalMarks = -99;
+
+            var collegeStudent = new CollegeStudent
+            {
+                Name = "College Student",
+                TotalMarks = -99 
+              
+            };
+
+            //Here we change the marks of this college student guy...so the event "TotalMarksHandler" gets fired again. 
+            collegeStudent.TotalMarks = 99;
+
+            Console.ReadKey();
+        }
+
+       
+       
+    }
+}
---
+++ EventHandlersDemo/UniversityStudent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandlersDemo
+{
+    class UniversityStudent
+    {
+
+        public UniversityStudent()
+        {
+            this.DisplayTotalMarks+=UniversityStudent_DisplayTotalMarks;
+        }
+
+        void UniversityStudent_DisplayTotalMarks( string name, int marks)
+        {
+            Console.WriteLine("{0} has scored {1}", name, marks);
+        }
+
+        private int totalMarks =0;
+
+        public string Name { get; set; }
+        public int TotalMarks {
+           set
+           {
+               totalMarks = value;
+
+                DisplayTotalMarks(this.Name, totalMarks);
+            }
+        }
+
+        public event DisplayTotalMarks DisplayTotalMarks;
+
+        
+    }
+
+    class CollegeStudent : UniversityStudent
+    {
+
+        public CollegeStudent( )
+        {
+            //Use this if you want to have a custom implementation of the same event
+           // this.DisplayTotalMarks+=CollegeStudent_DisplayTotalMarks;
+        }
+
+        //UnComment this if you would love to have a custom implementation.
+
+        //void CollegeStudent_DisplayTotalMarks( string name, int totalMarks )
+        //{
+        //    Console.WriteLine("In the College Student Event Handler");
+            
+        //}
+    }
+}
